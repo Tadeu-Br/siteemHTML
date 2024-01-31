f9ed28421e11a87ae7d25cc296d4006d235b0e4d
Tadeu Brasil <tadeu.brasil04@gmail.com>
2024-01-31 20:44:31 -0300

    entrada, junto com imagem, video e seleções, pra um servidor local em XAMPP

---
+++ htdocs/form.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+
+		<center>
+			<title>Nome</title>
+            <meta charset="UTF-8">
+            <meta name="description" content="Aqui é um site que o tadeu fez.">
+		</center>
+	</head>
+<body>
+<form action="" method="GET">
+    Nome: <input type="text" name="" placeholder="Nome" required=""><br>
+    Data de Nascimento: <input type="date" name=""><br>
+    Numero: <input type="number" name=""required=""><br>
+    Email: <input type="email" name=""><br>
+    Senha <input type="password" name=""><br>
+    Texto: <input type="text" name="" value="Hackersec" readonly="" disabled=""><br>
+
+
+    novo:
+    <select>
+        <option value="">sim</option>
+        <option value="">nao</option>
+        <option value="">talvez<option>
+    </select>
+
+    <input type="checkbox" name=""> Aceitar os termos<br>
+    <button type="submit">Enviar</button>
+
+</form>	
+</body>
+</html>
---
+++ htdocs/index.php
@@ -0,0 +1,49 @@
+<html>
+	<head>
+
+		<center>
+			<title>Hello Word</title>
+		</center>
+	</head>
+<body>
+		<h1>Hello World</h1>
+		<h2> h2 </h2>
+		<h3> h3 </h3>
+		<h4>h4 </h4>
+		<h5> h5 </h5>
+		<h6> h6 </h6>
+		<p>paragrafo1</p>
+		<p><b>paragrafo2</b></p>
+		<b>Negrito</b><br>
+		<i> Italico</i>
+		<br>
+		<a href="https://google.com/" target="t_blank" rel="noopener"><b>google, lol</b></a>
+		
+		<img alt="" width="1278" height="1278" src="https://images.wsj.net/im-605350/8SR" class="css-4v2gs4">
+		<br></br>
+		<img src="img/pentest.jpg">
+		<video width="320" height="430" controls>
+			<source src="video/mp4/minecraft.mp4" type="video/mp4">
+			<source src="video/mp4/minecraft.ogg" type="video/ogg">
+		
+		</video>
+		<audio>
+			<source src="video/mp4/minecraft.mp4" type="video/mp4">
+			<source src="video/mp4/minecraft.ogg" type="video/ogg">
+		
+		</audio>
+
+		<form action="" method="GET">
+			
+			Nome: <input type="text" name=""><br>
+			Data de nascimento: <input type="date"  name""><br>
+			Numero: <input type="number" name=""><br>
+			País:
+			<select name="pais"> 
+				<option value="">Brasil</option>
+				<option value="">Canada</option>
+				<option value="">EUA</option>
+			</select>
+			<input type="checkbox" name="carismatico"> termos aceitos?
+</body>
+</html>
